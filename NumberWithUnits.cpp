605f87b6b9671af7d9faad156e2da25f0eebaf33
Dolev .H <33119992+Dolev@users.noreply.github.com>
2021-04-21 16:24:01 +0300

    Update NumberWithUnits.cpp

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -1,32 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <bits/stdc++.h>
+#include <utility>
 #include "NumberWithUnits.hpp"
-using namespace ariel;
-        ostream& operator<<(ostream& os) {
-            return os << getParameter() << " [" << gettype() << "] "<<endl;
-        }
-        NumberWithUnits operator+(const NumberWithUnits& other) const {
-            return NumberWithUnits(this->parameter+other.getParameter(),if(this->type==other.gettype()){return this->type}else{throw invalid_argument("CASE 1 : Wrong input: not the smae type.");});
-        }
-        NumberWithUnits operator-(const NumberWithUnits& other) const {
-             return NumberWithUnits(this->parameter-other.getParameter(),if(this->type==other.gettype()){return this->type}else{throw invalid_argument("CASE 2 : Wrong input: not the smae type.");});
-        }
-        NumberWithUnits& operator++() {
-         return NumberWithUnits(++this->parameter,this->type);
-        }
-        NumberWithUnits& operator--(NumberWithUnits& a) {
-            a.setParamter()=a.getParamter()-1;
-            return a;
-        }
-        NumberWithUnits& operator+=(NumberWithUnits& a) {
-            return a;
-        }
-        NumberWithUnits& operator-=(NumberWithUnits& a) {
-            return a;
-        }
-        bool operator ==(NumberWithUnits a, NumberWithUnits b){
-            if(a.getParameter()==b.getParameter()){
-                 return true;
-            }
-            else{
-                 return false;
-            }
-        }
+using namespace std;
+
+namespace ariel{
+void NumberWithUnits::read_units(ifstream& file){
+}
+
+ostream& operator<<(ostream& output, const NumberWithUnits& other){
+    return output;// should print output << other.parameter << " [" << other.type << "] " << endl;
+}
+void operator>>(istringstream&input, NumberWithUnits& a){
+
+}
+
+NumberWithUnits operator+(NumberWithUnits& a, NumberWithUnits& b){
+   return NumberWithUnits {0, "km"};
+}
+
+NumberWithUnits& NumberWithUnits::operator+=(const NumberWithUnits& b){
+    return *this;
+}
+
+NumberWithUnits operator-(NumberWithUnits& a, NumberWithUnits& b){
+    return NumberWithUnits {0, "km"};
+}
+
+NumberWithUnits& NumberWithUnits::operator-=(NumberWithUnits& b){
+    return b;
+}
+
+NumberWithUnits NumberWithUnits:: operator-(){
+    return NumberWithUnits {0, "km"};
+}
+
+NumberWithUnits& operator*(NumberWithUnits& a, double x){
+    return x*a;
+}
+
+NumberWithUnits& operator*(double x, NumberWithUnits& a){
+    return a*x;
+}
+
+bool operator==(const NumberWithUnits& a, const NumberWithUnits& b){
+     return true;
+}
+
+bool operator!=(NumberWithUnits& a, NumberWithUnits& b){
+     return true;
+}
+
+bool operator<(NumberWithUnits& a, NumberWithUnits& b){
+    return true;
+}
+bool operator>(NumberWithUnits& a, NumberWithUnits& b){
+    return true;
+}
+bool operator<=(NumberWithUnits& a, NumberWithUnits& b){
+    return true;
+}
+bool operator>=(NumberWithUnits& a, NumberWithUnits& b){
+    return true;
+}
+
+// Postfix operator
+NumberWithUnits& NumberWithUnits::operator++(){
+    return *this;
+}
+NumberWithUnits& NumberWithUnits::operator--(){
+    return *this;
+}
+
+// Prefix operator
+NumberWithUnits NumberWithUnits::operator++(int temp){
+    NumberWithUnits a {0, "km"};
+         return a;
+}          
+NumberWithUnits NumberWithUnits::operator--(int temp){
+    NumberWithUnits a {0, "km"};
+         return a;
+}
+
+
+}
